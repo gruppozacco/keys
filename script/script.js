ac30726f7e611da39d0cef8aa4d8351146e2a687
gruppozacco <gruppozaccomarketing@gmail.com>
2023-06-08 15:02:12 +0200

    button

--- script/script.js
+++ script/script.js
@@ -8,13 +8,20 @@ const { createApp } = Vue
       return {
         message: 'Hello Vue!',
         contact: false,
+        password: "keysofsicily2023"
       }
     },
     methods: {
       showContact() {
         this.contact = !this.contact;
         console.log(this.contact);
-      }
+      },
+
+      copy() {
+        console.log(this.password)
+        navigator.clipboard.writeText(this.password);
+        alert("Testo copiato: " + this.password);
+    }
     },
     
   }).mount('#app')
