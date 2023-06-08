ad4612a1e594d7897beb1d3f73bfd4f4c25e6c3c
gruppozacco <gruppozaccomarketing@gmail.com>
2023-06-08 12:42:47 +0200

    contatti

--- script/script.js
+++ script/script.js
@@ -6,7 +6,15 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        message: 'Hello Vue!'
+        message: 'Hello Vue!',
+        contact: false,
       }
-    }
+    },
+    methods: {
+      showContact() {
+        this.contact = !this.contact;
+        console.log(this.contact);
+      }
+    },
+    
   }).mount('#app')
